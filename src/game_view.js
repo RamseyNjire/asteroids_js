a75cde145387699e316b1365bc9ec4ab2141b530
Ramsey Njire <ramseynjire@gmail.com>
2022-03-18 10:34:29 +0300

    write keyhandlers method in gameview

--- src/game_view.js
+++ src/game_view.js
@@ -9,10 +9,27 @@ GameView.prototype.start = function() {
     const game = this.game;
     const context = this.context;
     game.addAsteroids();
+    this.bindKeyHandlers();
     const intervalID = setInterval(function(){
         game.step();
         game.draw(context);
     }, 20);
 };
+
+GameView.prototype.bindKeyHandlers = function() {
+    const ship = this.game.ship;
+    key('w, up', function(){
+        ship.power([0, -1]);
+    });
+    key('d, right', function () {
+        ship.power([1, 0]);
+    });
+    key('s, down', function () {
+        ship.power([0, 1]);
+    });
+    key('a, left', function () {
+        ship.power([-1, 0]);
+    });
+}
 
 module.exports = GameView;
--- src/ship.js
+++ src/ship.js
@@ -23,5 +23,10 @@ Ship.prototype.relocate = function () {
     this.velocity = [0, 0];
 }
 
+Ship.prototype.power = function(impulse) {
+    this.velocity[0] += impulse[0];
+    this.velocity[1] += impulse[1];
+}
+
 
 module.exports = Ship;
